33dcbe5973d86a9c9f6f335867d6dd56ee917924
Kollu <skollu@saintpeters.edu>
2025-03-23 09:18:41 -0700

    Please ente the commit message for your changes. Lines starting

--- src/books/book_data.py
+++ src/books/book_data.py
@@ -1,9 +1,3 @@
-from fastapi import FastAPI, status
-from typing import List
-from pydantic import BaseModel
-from fastapi.exceptions import HTTPException
-app=FastAPI()
-
 books=[
   {
     "id": 1,
@@ -96,56 +90,3 @@ books=[
     "published_date": "1877-01-01"
   }
 ]
-
-class Book(BaseModel):
-    id:int
-    title: str
-    author: str
-    publisher: str
-    language: str
-    pagecount: int
-    published_date: str
-  
-class BookUpdateModel(BaseModel):
-    title: str
-    author: str
-
-
-@app.get("/books",response_model=List[Book])
-async def get_all_books():
-  return books
-
-@app.post("/books",status_code=status.HTTP_201_CREATED)
-async def create_book(book_data:Book)->dict:
-   new_book=book_data.model_dump()
-   books.append(new_book)
-   return new_book
-
-@app.get("/book/{book_id}")
-async def get_book(book_id:int):
-  for book in books:
-     if book["id"]==book_id:
-        return book
-     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                          detail="Book is not found in data base")
-
-@app.patch("/books/{book_id}")
-async def get_all_books(book_id:int,book_update_data:BookUpdateModel):
-  for book in books:
-     if book["id"]==book_id:
-        book["title"]=book_update_data.title
-        book["author"]=book_update_data.author
-
-        return book
-     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Bad request")
-  
-
-@app.delete("/book/{book_id}",status_code=status.HTTP_200_OK)
-async def delete_book(book_id:int):
-   for book in books:
-     if book["id"]==book_id:
-        books.remove(book)
-        return {
-           "message":"deleted data successfully"
-        }
-     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No data found with id")
